75390d58fbdf9194d3a4f3b8d378fac66093325b
xTheoxreborn <theopro23455@gmail.com>
2026-09-23 19:44:18 +0200

    Changement package en dll forge

--- src/Misty/AppInfo.cs
+++ src/Misty/AppInfo.cs
@@ -7,11 +7,9 @@ namespace Misty
     {
         /// <summary>Version du launcher. Doit correspondre au tag de la release GitHub (sans le "v")
         /// et au nom du dossier d'installation "Misty-{Version}".</summary>
-        public const string Version = "0.2.3.9.5.1";
-
+        public const string Version = "0.2.3.9.5.2";
         /// <summary>Version du format de data.txt. À incrémenter quand on ajoute/retire une clé.</summary>
         public const string DataVersion = "1.2.2";
-
         public const string GitHubRepo = "xTheoxreborn/Misty";
         public const string DiscordAppId = "1536551827640680528";
         public const int RamParDefautMo = 4096;
